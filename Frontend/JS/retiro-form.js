298a31399ec88c5211607a623548979eeb472b5e
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2021-05-25 13:46:47 -0300

    COOP-45 Formulario completo y funcional

--- Frontend/JS/retiro-form.js
+++ Frontend/JS/retiro-form.js
@@ -1,7 +1,34 @@
-document.querySelector("#testId").innerHTML=
-`
-<form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-<input type="search" class="form-control form-control-dark" placeholder="Buscar..."
-    aria-label="Search">
-</form>
-`
+
+let main = document.querySelector("#carousel")
+
+let response = await fetch('./js/retiro-form.html');
+if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
+    let mytext = await response.text();
+
+    main.innerHTML = mytext
+  }
+
+
+  document.querySelector('#photoFile').onchange = function() {
+    loadFile(event)
+  };
+
+
+document.querySelector('#photo').addEventListener("click",()=>{
+    document.querySelector("#photoFile").click()
+    
+})
+
+const loadFile = function(event) {
+    const output = document.querySelector('#img');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) 
+    }
+  };
+
+document.querySelector('#inscription').addEventListener("click",()=>{
+   console.log("inscripto")
+})
